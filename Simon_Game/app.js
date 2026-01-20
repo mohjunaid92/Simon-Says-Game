13635a81c755a6c8cd78e0e02c343e24e0ec39ee
Moh junaid malik <mohdjunaid74560@gmail.com>
2026-01-21 02:51:27 +0530

    ne w changes

--- Simon_Game/app.js
+++ Simon_Game/app.js
@@ -1,108 +1,93 @@
 let gameSeq = [];
 let userSeq = [];
-let colors = ["yellow", "red", "purple", "green"];
-let started = false;
-let level = 0;
 
+
+let colors = [ "yellow", "red", "purple", "green"];
 let h3 = document.querySelector("h3");
-let div5 = document.querySelector(".div5");
-let body = document.querySelector("body");
+let started = false;
+let Level = 0;
 
-
-document.addEventListener("keypress", function () {
-    if (started == false) {
-        console.log("game started");
+document.addEventListener("keypress", function(){
+    console.log("click");
+    if( started == false ){
         started = true;
-        levelUp();
+      levelUp();
+
 
     }
-
-
 });
+// Function that will start the game and generate the random color;
 
-function levelUp() {
+function levelUp(){
     userSeq = [];
-    level++;
-    h3.innerText = `Level ${level}`;
-    let index = Math.floor(Math.random() * 3);
-    let colClass = colors[index];
-    console.log(colClass);
-    let btn1 = document.querySelector("." + colClass);
-    gameSeq.push(colClass);
+    Level++;
+    h3.innerText = `Level ${Level}`
+    let randInd = Math.floor(Math.random()*4);
+    console.log(randInd);
+    let rColor = colors[randInd];
+    let btn = document.querySelector(`.${rColor}`);
+    gameSeq.push(rColor);
+    console.log(gameSeq)
 
-    btnFlash(btn1);
+     gameFlash(btn);
 
+   
+
+    
 }
-function btnFlash(btn) {
-
+// Game will flash a button 
+function gameFlash(btn){
     btn.classList.add("flash");
-    setTimeout(function () {
+    setTimeout(function(){
         btn.classList.remove("flash");
-
-
-    }, 400);
+    },200);
 }
-function userFlash(btn) {
 
+//When user will click n button then this function let blink
+function userFlash(btn){
     btn.classList.add("userflash");
-    setTimeout(function () {
+    setTimeout(function(){
         btn.classList.remove("userflash");
-
-
-    }, 200);
+    },200);
 }
 
+// user will be clicking on buttons to play game
 let btns = document.querySelectorAll(".btn");
-for (bt of btns) {
-    bt.addEventListener("click", function () {
-        console.log("button clicked");
-        console.log(this);
+for(btnn of btns){
+    btnn.addEventListener("click", function(){
+        userSeq.push(this.id);
+        console.log(userSeq);
         userFlash(this);
-      
-        userSeq.push(this.classList[0]);
-        checkAns(userSeq.length-1);
+        compare(userSeq.length-1);
+
     });
 }
 
-function checkAns(indx){
-   
-    console.log(level)
-    if( userSeq[indx] === gameSeq[indx]){
+// compare game Sequence and user sequence
+function compare(index){
+    if( userSeq[index] === gameSeq[index]){
         if( userSeq.length == gameSeq.length){
-           setTimeout(levelUp, 1000);
+            setTimeout(levelUp, 1000);
         }
+      
     } else {
-        bodyFlash()
-        h3.innerText = `Game over! Press any key to start`;
-        let newh3 = document.querySelector("h3");
-        newh3.innerText = `Your score was ${level}`;
-        body.append(newh3);
+            h3.innerText = "Game Over try again";
+            let body = document.querySelector("body");
+            bodyFlash(body);   
+            let newh3 = document.createElement("h3");
+            newh3.innerText =`Your score is ${Level}`
+            body.append(newh3);
+        }
 
-
-         }
-
-} 
-function bodyFlash() {
-  
-
-    body.classList.add("redd");
-    setTimeout(function () {
-        body.classList.remove("redd");
-
-
-    }, 200);
+    
 }
-console.log(gameSeq)
-
-   
-
-
-
-
-
-
-
-
+//when user will click on wron button
+function bodyFlash(btn){
+    btn.classList.add("redd");
+    setTimeout(function(){
+        btn.classList.remove("redd");
+    },200);
+}
 
 
 
